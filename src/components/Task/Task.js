794c060e4db75aade3fcf6f3c4b03c36e077da6a
Georgiy Antonevich <goshka.ruu@mail.ru>
2022-07-17 11:09:38 +0300

    add
    Signed-off-by: Georgiy Antonevich <goshka.ruu@mail.ru>

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,29 +1,29 @@
-import React, { Component } from "react";
-import "./Task.css";
-import TaskInfo from "./TaskInfo";
+import React, { Component } from 'react';
 
-export default class Task extends Component
-{
-    render ()
+import './Task.css';
+import TaskInfo from './TaskInfo';
+
+export default class Task extends Component {
+  render ()
+  {
+    const { todos, onDeleted, onToggleDone, onToggleImportant } = this.props
+
+    const elements = todos.map(function (el)
     {
-        const { todos, onDeleted, onToggleDone, onToggleImportant } = this.props
-
-        const elements = todos.map(function (el)
-        {
-            const { id, ...itemProps } = el
-            return (
-                <
-                    TaskInfo
-                    key={ id }
-                    { ...itemProps }
-                    onDeleted={ () => onDeleted(id) }
-                    onToggleImportant={ () => onToggleImportant(id) }
-                    onToggleDone={ () => onToggleDone(id) }
-                />
-            )
-        })
-        return (
-            <ul className="todo-list">{ elements }</ul>
-        )
-    }
+      const { id, ...itemProps } = el
+      return (
+        <
+          TaskInfo
+          key={ id }
+          { ...itemProps }
+          onDeleted={ () => onDeleted(id) }
+          onToggleImportant={ () => onToggleImportant(id) }
+          onToggleDone={ () => onToggleDone(id) }
+        />
+      )
+    })
+    return (
+      <ul className="todo-list">{ elements }</ul>
+    )
+  }
 }
